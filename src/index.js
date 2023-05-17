af7227bf9220333a792b77c894d2f2c8663d28c7
narinakara <karagevuryan04@bk.ru>
2023-05-17 22:24:37 +0300

    Add change

--- src/index.js
+++ src/index.js
@@ -6,3 +6,19 @@ import './styles/main.scss'
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
