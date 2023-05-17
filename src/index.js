553f7d96af049321ee00b6f162b90483cedb7eec
narinakara <karagevuryan04@bk.ru>
2023-05-17 13:49:39 +0300

    Add change

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import 'normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss'
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
